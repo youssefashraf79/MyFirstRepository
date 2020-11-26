2c12f138d7551b7d2c90122f9216354b692e006c
youssefashraf79 <75047795+youssefashraf79@users.noreply.github.com>
2020-11-26 17:52:24 +0200

    Add files via upload

---
+++ agile1/agilre1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+ void main ()
+{
+
+	cout << "hello world 1 !" << endl;
+	cout << "hello world 2 !" << endl;
+	cout << "hello world 3 !" << endl;
+
+
+	system("pause");
+}
